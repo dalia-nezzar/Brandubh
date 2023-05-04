ab1752ea0c996d754ca2efd2b052d6d9d79f948f
dnezzar-iut90 <dalia.nezzar@edu.univ-fcomte.fr>
2023-05-04 14:13:27 +0200

    Upgraded Brandubh's introduction
    A new update regarding the players' name condition have been added. Subject to later changes.

--- src/BrandubhConsole.java
+++ src/BrandubhConsole.java
@@ -69,13 +69,13 @@ public class BrandubhConsole {
             String player1 = getName();
             model.addHumanPlayer(player1);
             String player2 = getName();
-            if (player1.equals(player2)) {
+            if (player1.toLowerCase().equals(player2.toLowerCase())) {
                 System.out.println(RED_BOLD+"You can't have two warriors with the same name!"+BLACK);
                 do{
-                    System.out.println("Enter a new name for the second player, son! Else... The war will never start! ");
+                    System.out.println(BLACK_BOLD+"Enter a new name for the second player, son! Else... The war will never start! "+BLACK);
                     player2 = getName();
                 }
-                while (player1.equals(player2));
+                while (player1.toLowerCase().equals(player2.toLowerCase()));
             }
             model.addHumanPlayer(player2);
         }
@@ -114,7 +114,7 @@ public class BrandubhConsole {
     static String getName() {
         String name = "";
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Enter your name, warrior ! ");
+        System.out.println("Enter your name, warrior! ");
         try {
             name = br.readLine();
         }
@@ -132,22 +132,25 @@ public class BrandubhConsole {
         while (true) {
             try {
                 answer = br.readLine();
-                if (answer.equals("yes")
-                || answer.equals("y")
-                || answer.equals("yeah")
-                || answer.equals("yep")
-                || answer.equals("yup")
-                || answer.equals("ye")
-                || answer.equals("yee")
-                || answer.equals("oui")) {
+                if (answer.toLowerCase().equals("yes")
+                || answer.toLowerCase().equals("y")
+                || answer.toLowerCase().equals("yeah")
+                || answer.toLowerCase().equals("yep")
+                || answer.toLowerCase().equals("yup")
+                || answer.toLowerCase().equals("ye")
+                || answer.toLowerCase().equals("yee")
+                || answer.toLowerCase().equals("oui")
+                || answer.toLowerCase().equals("1")) {
                     System.out.println("Fair. Let's head to the warzone, ey!");
+                    System.out.println("=======================================");
                     break;
-                } else if (answer.equals("no")
-                || answer.equals("n")
-                || answer.equals("nope")
-                || answer.equals("nah")
-                || answer.equals("non")
-                || answer.equals("noo")) {
+                } else if (answer.toLowerCase().equals("no")
+                || answer.toLowerCase().equals("n")
+                || answer.toLowerCase().equals("nope")
+                || answer.toLowerCase().equals("nah")
+                || answer.toLowerCase().equals("non")
+                || answer.toLowerCase().equals("noo")
+                || answer.toLowerCase().equals("0")) {
                     System.out.println("Oh, that's too bad, son. Let me explain them to you then.");
                     rulesBrandubh();
                     break;
@@ -158,7 +161,7 @@ public class BrandubhConsole {
                 System.out.println("There was an error reading your answer (rules). Try again, son.");
             }
         }
-        System.out.println(BLUE_BOLD + "Speak, son, what kind of game would you like to play?" + BLACK);
+        System.out.println(BLUE_BOLD + "Speak, son, what kind of war would you like to engage yourself in?" + BLACK);
         System.out.println("Write " + RED_BOLD +HUMAN_VS_HUMAN + BLACK+" for a brother (human) vs brother (human) war,");
         System.out.println("or, write "+ RED_BOLD + HUMAN_VS_COMPUTER + BLACK+" for a brother (human) vs God (computer) war!");
         System.out.println("or, write " + RED_BOLD + COMPUTER_VS_COMPUTER + BLACK+" for a God (computer) vs God (computer) war!");
@@ -186,15 +189,15 @@ public class BrandubhConsole {
             Thread.sleep(2000);
             System.out.println("A "+RED_BOLD+"WAR"+BLACK+", was declared on this 7x7 territory. You can either"+ GREEN_BOLD+" DEFEND"+BLACK+" your Branan or"+ RED_BOLD+" ATTACK"+BLACK +" the enemy's Branan.");
             Thread.sleep(5000);
-            System.out.println("BUT, beware... If you wish to defend the Branan, you and your brothers will only be "+ GREEN_BOLD+"four"+BLACK+", and your enemy will possess"+ BLACK+ RED_BOLD+" eight "+BLACK +"warriors!");
+            System.out.println("BUT, beware... If you wish to defend the Branan, you and your brothers will only be "+ GREEN_BOLD+"four"+BLACK+", while your enemy will possess"+ BLACK+ RED_BOLD+" eight "+BLACK +"warriors!");
             Thread.sleep(3000);
-            System.out.println("The end of this war is proclaimed if "+ RED_UNDERLINED+"the King gets captured between two enemy warriors"+BLACK+", or if "+GREEN_UNDERLINED+"the King gets to one of the corners of the board."+BLACK);
+            System.out.println("The end of this war is proclaimed if "+ RED_UNDERLINED+"the Branan gets captured between two enemy warriors"+BLACK+", or if "+GREEN_UNDERLINED+"the Branan gets to one of the corners of the board."+BLACK);
             Thread.sleep(5000);
-            System.out.println("To capture an enemy, you'll have to surround one of them between "+ PURPLE_BOLD+"two of your warriors. "+BLACK+ BLACK_UNDERLINED+"If the enemy enters volonteeringly between two of your warriors, he will NOT be captured."+BLACK);
+            System.out.println("To capture an enemy, you'll have to surround one of them between "+ PURPLE_BOLD+"two of your warriors. "+BLACK+ BLACK_UNDERLINED+"If one enemy soldier enters volonteeringly between two of your warriors, he will NOT be captured."+BLACK);
             Thread.sleep(8000);
             System.out.println(PURPLE_BOLD+"The corners and the center of the board are unaccessible for all the warriors."+BLACK+" "+ GREEN_BOLD+"Only the King can get there."+BLACK);
             Thread.sleep(3000);
-            System.out.println("And if the King steps out of the center, his throne, he can" +GREEN_BOLD+" no longer return to it.");
+            System.out.println("And if the Branan steps out of the center, his throne, he can" +GREEN_BOLD+" no longer return to it.");
             System.out.println(BLACK_BOLD+"============= END OF THE RULES =============");
             System.out.println("");
         }
