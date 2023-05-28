18079b2762ecc512e2f186500c65608d2a0a0479
diarrathiamm <119352100+diarrathiamm@users.noreply.github.com>
2023-05-28 20:00:32 +0200

    Test v2 work X Théo
    We want to die but finally works so we are happy

--- test/control/TestBRBController.java
+++ test/control/TestBRBController.java
@@ -3,26 +3,23 @@ package control;
 import boardifier.model.Model;
 import boardifier.view.View;
 import model.BRBStageModel;
-import control.BRBController;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class TestBRBController{
     // assuming we have a suitable class and method to set up the model and other dependencies
-    private Model model;
-    private BRBStageModel gameStage;
-    private BRBController controller;
-    private View view;
+    Model model;
+    BRBStageModel gameStage;
+    BRBController controller;
+    View view;
 
     @Before
     public void setup() {
         model = new Model();
-        gameStage = new BRBStageModel("game", model);
-        model.setGameStage(gameStage);
-        view = new View(model);
+        gameStage = new BRBStageModel("model",model);
         // Do any necessary setup here. Instantiate the model, the gameStage etc.
+        view = new View(model);
         controller = new BRBController(model, view);
     }
 
@@ -47,15 +44,5 @@ public class TestBRBController{
         assertFalse(controller.startanalyseAndPlay(line));
     }
 
-    // assuming the MoveAction and other classes are correctly set up,
-    // we could test a successful scenario as well
-    @Test
-    public void testAnalyseAndPlayWithValidInput() {
-        model.setIdPlayer(0);
-        String line = "1A1";
-        // this test depends on the initial state of the board
-        // and the methods like getCoords(), setValidCells(), canReachCell(), etc.
-        // assuming they work correctly, this test should pass if the pawn can be moved to A1
-        assertTrue(controller.startanalyseAndPlay(line));
-    }
+
 }
