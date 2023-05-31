271cc27a947c6d12e0d8df7101d1311616994c36
Noah Nicolas <noahesteve.nicolas@gmail.com>
2023-05-31 16:16:19 +0200

    fix for inputfiles

--- src/GameLauncher.java
+++ src/GameLauncher.java
@@ -1,3 +1,5 @@
+import control.BRBController;
+
 import java.util.Scanner;
 
 public class GameLauncher {
@@ -6,20 +8,18 @@ public class GameLauncher {
         System.out.println("1. Console Version");
         System.out.println("2. Graphical Version");
 
-        Scanner scanner = new Scanner(System.in);
-        int choice = scanner.nextInt();
+        String choice = BRBController.input.nextLine().replaceAll("\\D+", "");
+        int choiceInt = Integer.parseInt(choice);
 
-        if (choice == 1) {
+        if (choiceInt == 1) {
             // Execute the console version
             BrandubhConsole game = new BrandubhConsole();
             game.start(args);
-        } else if (choice == 2) {
+        } else if (choiceInt == 2) {
             // Execute the graphical version
             //JavaFXGame.launch(JavaFXGame.class, args);
         } else {
             System.out.println("Invalid choice. Exiting the game.");
         }
-
-        scanner.close();
     }
 }
